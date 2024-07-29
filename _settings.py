6d2160bd46c35d67c710686d7ea11fd7f2e553af
ramneetk <33604200+ramneetk@users.noreply.github.com>
2024-07-29 18:14:43 -0400

    Update _settings.py

--- _settings.py
+++ _settings.py
@@ -16,11 +16,11 @@ os.makedirs(GENERATION_FOLDER, exist_ok=True)
 # After running pipeline/generate.py, update the following paths to the generated files if necessary.
 GEN_PATHS = {
     'coqa': {
-        'llama-13b': f'{GENERATION_FOLDER}/llama-13b-hf_coqa_10/0.pkl', #llama-2-13B
-        'mistral-7b': f'{GENERATION_FOLDER}/mistral-7b-hf_coqa_10/0.pkl', # Mistral-7B-Instruct-v0.2
+        'llama-13b-hf': f'{GENERATION_FOLDER}/llama-13b-hf_coqa_10/0.pkl', #llama-2-13B
+        'mistral-7b-hf': f'{GENERATION_FOLDER}/mistral-7b-hf_coqa_10/0.pkl', # Mistral-7B-Instruct-v0.2
    },
    'triviaqa': {
-        'llama-13b': f'{GENERATION_FOLDER}/llama-13b-hf_triviaqa_0/0.pkl',
-        'mistral-7b': f'{GENERATION_FOLDER}/mistral-7b-hf_triviaqa_10/0.pkl',  
+        'llama-13b-hf': f'{GENERATION_FOLDER}/llama-13b-hf_triviaqa_0/0.pkl',
+        'mistral-7b-hf': f'{GENERATION_FOLDER}/mistral-7b-hf_triviaqa_10/0.pkl',  
    }
 }
